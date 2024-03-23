aea86e05d1eb930966d908a4b2bcf9b342ee5646
DanilTsapok <daniltsapok12012004@gmail.com>
2024-03-23 14:55:15 +0300

    Start

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  let swaggerConfig = new DocumentBuilder()
+    .setTitle('RedComp')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,45 @@
+import { Get, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from './entities/user.entity';
+import { Repository } from 'typeorm';
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRespository: Repository<User>,
+  ) {}
+
+  async getByEmail(email: string) {
+    const user = await this.userRespository.findOne({ where: { email } });
+    if (user) {
+      return user;
+    }
+    throw new HttpException(
+      'User with this email doesn`t exist',
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  async create(userData: CreateUserDto) {
+    const newUser = await this.userRespository.create(userData);
+    await this.userRespository.save(newUser);
+    return newUser;
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
