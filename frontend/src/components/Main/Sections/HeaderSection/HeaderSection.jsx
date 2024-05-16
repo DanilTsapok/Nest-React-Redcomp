7ff9d1ae9616be63abdc85f9d6b1e72417b94b5c
DanilTsapok <daniltsapok12012004@gmail.com>
2024-05-16 20:53:28 +0300

    Logout

--- frontend/src/components/Main/Sections/HeaderSection/HeaderSection.jsx
+++ frontend/src/components/Main/Sections/HeaderSection/HeaderSection.jsx
@@ -22,12 +22,34 @@ function HeaderSection() {
             </div>
           </div>
           {activeBtnLogin ? (
-            <button
-              className={style.btnLogin}
-              onClick={() => setAuthModalActive()}
-            >
-              Login
-            </button>
+            // <button
+            //   className={style.btnLogin}
+            //   onClick={() => setAuthModalActive()}
+            // >
+            //   Login
+            // </button>
+            <div className={style.btnLogin}>
+              <button
+                className={style.cssbuttons_io_button}
+                onClick={() => setAuthModalActive()}
+              >
+                Login
+                <div className={style.icon}>
+                  <svg
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path
+                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
           ) : (
             <button
               className={style.btnLogin}
