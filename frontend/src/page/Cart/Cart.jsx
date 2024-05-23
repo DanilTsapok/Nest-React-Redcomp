7d81b6e0267bc36e954c1847074667e383c2802a
DanilTsapok <daniltsapok12012004@gmail.com>
2024-05-23 09:01:02 +0300

    commit

--- frontend/src/page/Cart/Cart.jsx
+++ frontend/src/page/Cart/Cart.jsx
@@ -1,13 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InfiniteSlide from "../../components/Main/Sections/InfiniteSlide/infiniteSlide";
 import style from "./cartstyle.module.scss";
 import video from "../../assets/video2.mp4";
+import useStore from "../../store/useStore";
+import axios from "axios";
 function Cart() {
+  const { currentUser } = useStore();
+  const [orders, setOrders] = useState([]);
+  const [orderItems, setOrderItems] = useState([]);
+  console.log("Orders", orders);
+  console.log("OrderItems", orderItems);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const responseOrders = await axios.get(`http://localhost:4000/order`);
+        setOrders(responseOrders.data);
+
+        const responseOrderItems = await axios.get(
+          `http://localhost:4000/order-items`
+        );
+        setOrderItems(responseOrderItems.data);
+      } catch (error) {
+        console.error("Error fetching orders or order items:", error);
+      }
+    };
+
+    if (currentUser) {
+      fetchOrders();
+    }
+  }, [currentUser]);
+
+  // Функция для поиска уникальных заказов по идентификатору
+  const findUniqueOrders = () => {
+    const uniqueOrders = [];
+    orders.forEach((order) => {
+      if (!uniqueOrders.find((uniqueOrder) => uniqueOrder.id === order.id)) {
+        uniqueOrders.push(order);
+      }
+    });
+    return uniqueOrders;
+  };
+
   return (
     <div className={style.CartBody}>
       <video src={video} loop autoPlay muted></video>
       <h1>Cart</h1>
-      <div className={style.cartBody}></div>
+      <div className={style.cartBody}>
+        {orders.length > 0 ? (
+          findUniqueOrders().map((singleOrder) => (
+            <div className={style.orderDetails} key={singleOrder.id}>
+              <h2>Order Details</h2>
+              <p>Order ID: {singleOrder.id}</p>
+              <p>
+                Order Date: {new Date(singleOrder.date).toLocaleDateString()}
+              </p>
+              <h3>Items:</h3>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+        <ul>
+          {orderItems
+            // .filter((itemOrder) => itemOrder.orderId == singleOrder)
+            .map((item) => (
+              <li key={item.id}>
+                <img src={item.product.imgUrl} alt="" />
+                <p>Product ID: {item.product.id}</p>
+                <p>Order ID: {item.order.id}</p>
+                <p>Product Name: {item.product.name}</p>
+                <p>Product Price: {item.product.price}</p>
+                <p>Quantity: {item.quantity}</p>
+              </li>
+            ))}
+        </ul>
+      </div>
       <InfiniteSlide />
     </div>
   );
--- frontend/src/page/ProductPage/ProductsPage.jsx
+++ frontend/src/page/ProductPage/ProductsPage.jsx
@@ -1,59 +1,86 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import style from "./productsPageStyle.module.scss";
 import useStore from "../../store/useStore";
 import video from "../../assets/video2.mp4";
-import { Col, Image, Row, Statistic } from "antd";
+import { Image } from "antd";
 import AddProductModal from "../../components/ModalWindows/AddProductModal/AddProductModal";
 import AddEditProductModal from "../../components/ModalWindows/AddEditModal/AddEditProductModal";
 import InfiniteSlide from "../../components/Main/Sections/InfiniteSlide/infiniteSlide";
+
 function ProductsPage() {
   const { currentUser, setAddProductModalActive, setEditCategoryModalActive } =
     useStore();
   const { categoryId } = useParams();
   const [categoryName, setCategoryName] = useState();
   const [products, setProducts] = useState([]);
+  const [orderId, setOrderId] = useState(null);
+
+  console.log(orderId);
+
   useEffect(() => {
     window.scrollTo(0, 0);
     const handleProducts = async () => {
       const response = await axios.get(
         `http://localhost:4000/category/${categoryId}/products`
       );
-
       const responseCategory = await axios.get(
         `http://localhost:4000/category/${categoryId}`
       );
       setCategoryName(responseCategory.data.name);
-      console.log(categoryName);
-
       setProducts(response.data);
-      console.log(response);
     };
     handleProducts();
-  }, [categoryName]);
+  }, [categoryId]);
 
   const DeleteProduct = async (idProduct) => {
     await axios.delete(`http://localhost:4000/product/delete/${idProduct}`);
-    window.location.replace(`/category/${categoryId}/products`);
+    window.location.reload();
   };
+
+  const CreateOrder = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/order", {
+        userId: currentUser.id,
+        date: new Date().toISOString(),
+      });
+      console.log(response);
+      setOrderId(response.data.id);
+      return response.data.id;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const CreateOrderItem = async (productId) => {
+    try {
+      const id = await CreateOrder();
+      await axios.post("http://localhost:4000/order-items", {
+        orderId: id,
+        productId: productId,
+        quantity: 1,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className={style.ProductsBody}>
       <video src={video} loop autoPlay muted></video>
       <h1>{categoryName}</h1>
       <div className={style.productBody}>
-        {products.length != 0 ? (
-          products.map((productsItem, index) => {
-            console.log(productsItem);
-            const delay = `${index * 0.1}s`; // Adjust the multiplier for desired delay
-            return (
-              <div
-                className={`${style.card}`}
-                style={{ "--delay": delay }}
-                key={index}
-              >
-                {currentUser ? (
-                  currentUser.roles.includes("Admin") ? (
+        {products.length
+          ? products.map((productsItem, index) => {
+              const delay = `${index * 0.1}s`;
+              return (
+                <div
+                  className={`${style.card}`}
+                  style={{ "--delay": delay }}
+                  key={index}
+                >
+                  {currentUser && currentUser.roles.includes("Admin") && (
                     <div className={style.AdminPos}>
                       <button onClick={() => DeleteProduct(productsItem.id)}>
                         <img
@@ -68,82 +95,53 @@ function ProductsPage() {
                           width="25"
                           height="25"
                           src="https://img.icons8.com/emoji/48/pencil-emoji.png"
-                          alt="trash"
+                          alt="edit"
                         />
                       </button>
                     </div>
-                  ) : (
-                    <div
-                      className={style.AdminPos}
-                      style={{ visibility: "hidden" }}
+                  )}
+                  <div style={{ zIndex: 999, width: "100%", height: "100%" }}>
+                    <Image.PreviewGroup>
+                      <Image
+                        className={style.img}
+                        src={productsItem.imgUrl}
+                        alt={productsItem.name}
+                      />
+                    </Image.PreviewGroup>
+                  </div>
+                  <p>{productsItem.name}</p>
+                  {productsItem.description.split(";").map((desc, i) => (
+                    <p key={i} style={{ fontSize: 16 }}>
+                      {desc}
+                    </p>
+                  ))}
+                  <p>{productsItem.price} UAH</p>
+                  {currentUser ? (
+                    <button
+                      className={style.BtnBuy}
+                      onClick={() => CreateOrderItem(productsItem.id)}
                     >
-                      <button onClick={() => DeleteProduct(productsItem.id)}>
-                        <img
-                          width="25"
-                          height="25"
-                          src="https://img.icons8.com/parakeet/48/trash.png"
-                          alt="trash"
-                        />
-                      </button>
-                      <button onClick={() => DeleteProduct(productsItem.id)}>
-                        <img
-                          width="25"
-                          height="25"
-                          src="https://img.icons8.com/emoji/48/pencil-emoji.png"
-                          alt="trash"
-                        />
-                      </button>
-                    </div>
-                  )
-                ) : (
-                  <></>
-                )}
-                <div style={{ zIndex: 999, width: "100%", height: "100%" }}>
-                  <Image.PreviewGroup>
-                    <Image
-                      className={style.img}
-                      src={productsItem.imgUrl}
-                      alt={productsItem.name}
-                    />
-                  </Image.PreviewGroup>
+                      Buy
+                    </button>
+                  ) : (
+                    <p style={{ margin: 20 }}>You need to login</p>
+                  )}
                 </div>
-
-                <p>{productsItem.name}</p>
-                {productsItem.description.split(";").map((desc, i) => (
-                  <p key={i} style={{ fontSize: 16 }}>
-                    {desc}
-                  </p>
-                ))}
-                <p>{productsItem.price} UAH</p>
-                {currentUser ? (
-                  <button className={style.BtnBuy}>Buy</button>
-                ) : (
-                  <p style={{ margin: 20 }}>You need to login</p>
-                )}
-              </div>
-            );
-          })
-        ) : (
-          <></>
-        )}
-        {currentUser ? (
-          currentUser.roles.includes("Admin") ? (
-            <div
-              className={style.card}
-              style={{
-                height: "450px",
-                display: "flex",
-                justifyContent: "center",
-              }}
-              onClick={() => setAddProductModalActive()}
-            >
-              <p>Add Product</p>
-            </div>
-          ) : (
-            <></>
-          )
-        ) : (
-          <></>
+              );
+            })
+          : null}
+        {currentUser && currentUser.roles.includes("Admin") && (
+          <div
+            className={style.card}
+            style={{
+              height: "450px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            onClick={() => setAddProductModalActive()}
+          >
+            <p>Add Product</p>
+          </div>
         )}
       </div>
       <InfiniteSlide />
