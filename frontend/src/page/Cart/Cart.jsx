c2e9cd3614406c43becd3fef6fdd032b9987d294
DanilTsapok <daniltsapok12012004@gmail.com>
2024-05-23 09:34:34 +0300

    commit

--- frontend/src/page/Cart/Cart.jsx
+++ frontend/src/page/Cart/Cart.jsx
@@ -4,12 +4,11 @@ import style from "./cartstyle.module.scss";
 import video from "../../assets/video2.mp4";
 import useStore from "../../store/useStore";
 import axios from "axios";
+
 function Cart() {
   const { currentUser } = useStore();
   const [orders, setOrders] = useState([]);
   const [orderItems, setOrderItems] = useState([]);
-  console.log("Orders", orders);
-  console.log("OrderItems", orderItems);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -31,6 +30,19 @@ function Cart() {
     }
   }, [currentUser]);
 
+  const deleteItem = async (idOrder, idItem) => {
+    try {
+      await axios.delete(`http://localhost:4000/order-items/${idItem}`);
+      if (idOrder) {
+        await axios.delete(`http://localhost:4000/order/${idOrder}`);
+      }
+      const updatedOrderItems = orderItems.filter((item) => item.id !== idItem);
+      setOrderItems(updatedOrderItems);
+    } catch (error) {
+      console.error("Error deleting order item:", error);
+    }
+  };
+
   // Функция для поиска уникальных заказов по идентификатору
   const findUniqueOrders = () => {
     const uniqueOrders = [];
@@ -48,15 +60,21 @@ function Cart() {
       <h1>Cart</h1>
       <div className={style.cartBody}>
         <div className={style.OrderBody}>
+          <h2>History Orders</h2>
           {orders.length > 0 ? (
             findUniqueOrders().map((singleOrder) => (
               <div className={style.orderDetails} key={singleOrder.id}>
-                <h2>Order Details</h2>
+                <h3>Order Details</h3>
                 <p>Order ID: {singleOrder.id}</p>
                 <p>
                   Order Date: {new Date(singleOrder.date).toLocaleDateString()}
                 </p>
-                <button>Delete</button>
+                <button
+                  onClick={() => deleteItem(singleOrder.id)}
+                  style={{ visibility: "hidden" }}
+                >
+                  Delete
+                </button>
               </div>
             ))
           ) : (
@@ -65,18 +83,21 @@ function Cart() {
         </div>
         <div className={style.cartOrderItem}>
           <ul>
-            {orderItems
-              // .filter((itemOrder) => itemOrder.orderId == singleOrder)
-              .map((item) => (
-                <li key={item.id}>
-                  <img src={item.product.imgUrl} alt="" />
-                  <p>Order ID: {item.order.id}</p>
-                  {/* <p>Product ID: {item.product.id}</p> */}
-                  <p>Product Name: {item.product.name}</p>
-                  <p>Product Price: {item.product.price} UAH</p>
-                  <p>Quantity: {item.quantity}</p>
-                </li>
-              ))}
+            {orderItems.map((item) => (
+              <li key={item.id}>
+                <img src={item.product.imgUrl} alt="" />
+                <p>Order ID: {item.order.id}</p>
+                <p>Product Name: {item.product.name}</p>
+                <p>Product Price: {item.product.price} UAH</p>
+                <p>Quantity: {item.quantity}</p>
+                <button
+                  onClick={() => deleteItem(item.orderId, item.id)}
+                  className={style.btnDelete}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
